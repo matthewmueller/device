79510d911185997ecd432bfb4e9c20e4a0ba0dde
Matt Mueller <mattmuelle@gmail.com>
2013-04-05 04:02:33 -0700

    initial

--- Readme.md
+++ Readme.md
@@ -1,15 +1,63 @@
 
 # device
 
-  high-level device io
+  Higher-level API built on matthewmueller/io built for separating out socket logic and sending messages between clients.
 
 ## Installation
 
     $ component install matthewmueller/device
 
+## Example
+
+```js
+var device = require('device')('http://socketserver.com');
+
+var batter = device('batter').type('mobile');
+var pitcher = device('pitcher').type('mobile');
+var field = device('field').type('desktop');
+
+batter.ready(function() {
+  batter.send('field', 'homerun');
+});
+
+pitcher.ready(function() {
+  pitcher.emit('throw');
+});
+
+field.ready(fn);
+```
+
 ## API
 
-   
+### `require('device')(url)`
+
+Initialize the devices with the given `url`. This is the url that gets passed into `matthewmueller/io` and ultimately into `learnboost/engine.io`.
+
+### `Device(name)`
+
+Initialize a `Device` with a given `name`. This name should uniquely identify the device from the rest of the devices in the room.
+
+### `.type(mobile|desktop|tablet)`
+
+Target a specific device. Maybe you want the desktop to be the view for input from your phone.
+
+### `.ready(fn)`
+
+Initializes the socket and calls `fn` when done. This library uses socket pooling in order for each connected client to call at most one `ready` function.
+
+## Socket Server
+
+There are some assumptions made about the configuration of the socket server. Head over to [matthewmueller/ws.mat.io](http://github.com/matthewmueller/ws.mat.io) to see the server-side component.
+
+## Test
+
+You'll need to edit the `test.html` to point to your socket server's address. Then do:
+
+```
+make
+serve &
+open http://localhost:3000
+```
 
 ## License
 
--- index.js
+++ index.js
@@ -8,6 +8,30 @@ var parse = require('user-agent-parser'),
     qs = require('querystring');
 
 /**
+ * List of devices
+ */
+
+var devices = [];
+
+/**
+ * Are we trying to open a connection?
+ */
+
+var opening = false;
+
+/**
+ * Have we connected to something?
+ */
+
+var connected = false;
+
+/**
+ * Window hash
+ */
+
+var hash = window.location.hash;
+
+/**
  * Expose `Device`
  */
 
@@ -20,25 +44,40 @@ module.exports = function(url) {
  * Initialize a `Device`
  *
  * var desktop = device('desktop')
- * var pitcher = device('pitcher', 'mobile');
- * var batter = device('batter', 'tablet');
+ * var pitcher = device('pitcher');
+ * var batter = device('batter');
  *
  * @param {String} name
- * @param {String} type
  * @param {Object} opts
  * @return {Device}
  * @api public
  */
 
-function Device(name, type, opts) {
-  if(!(this instanceof Device)) return new Device(name, type, opts);
+function Device(name, opts) {
+  if(!(this instanceof Device)) return new Device(name, opts);
   opts = opts || {};
   this.name = name;
-  this.type = type || name;
-  this.uid = this._uid();
-  this.ua = this._ua();
+  this.master = !hash;
+  this._uid = this.uid();
   this.pathname = window.location.pathname;
+  this.url = this._url();
+
+  // add to all devices
+  // devices.push({ type : type, name : name });
 }
+
+/**
+ * Give a device type
+ *
+ * @param {String} type
+ * @return {Device}
+ * @api public
+ */
+
+Device.prototype.type = function(type) {
+  this._type = type;
+  return this;
+};
 
 /**
  * Get the device's user agent
@@ -59,9 +98,24 @@ Device.prototype._ua = function() {
  * @api private
  */
 
-Device.prototype._uid = function() {
+Device.prototype.uid = function() {
   var hash = window.location.hash;
   return (hash) ? hash.slice(1) : uid(4);
+};
+
+/**
+ * Create the url
+ *
+ * @return {String} url
+ * @api private
+ */
+
+Device.prototype._url = function() {
+  // Add in query params
+  var obj = qs.parse(window.location.search);
+  obj.name = this.name;
+  var q = qs.stringify(obj);
+  return Device.url + '?' + q;
 };
 
 /**
@@ -73,24 +127,67 @@ Device.prototype._uid = function() {
  */
 
 Device.prototype.ready = function(fn) {
-  if(this.type != this.ua) return this;
+  // if the user agent doesnt match the type or we're already connected
+  // don't go any further
+  if(this._type && this._type != this._ua() || connected) return this;
 
-  // Add in query params
-  var obj = qs.parse(window.location.search);
-  obj.name = this.name;
-  var q = qs.stringify(obj);
+  this.fn = fn;
+  devices.push(this);
 
-  // ws.mat.io:80/color-tilt/1z3a?name=pitcher
-  var url = Device.url + '?' + q;
-  var io = this.io = IO(url);
+  // try to open socket connection
+  this.open();
 
-  // mixin io
-  for(var key in io) this[key] = io[key];
+  return this;
+};
 
-  // call "ready" when socket is opened
+/**
+ * Try to open with an identity (name).
+ * Open a connection one at a time.
+ * If the identity is already taken,
+ * try the next one.
+ *
+ * @param {Function} fn
+ * @return {Device}
+ * @api private
+ */
+
+Device.prototype.open = function() {
+  if(opening || connected) return this;
+  opening = true;
+  var self = this;
+  var device = devices.shift();
+
+  if(!device) return this;
+  var io = IO(device.url);
+
   io.socket.on('open', function() {
-    fn.call(this);
+
+    // identity acknowledged by socket server
+    io.on('ack', function() {
+      // mixin io
+      for(var key in io) device[key] = io[key];
+      opening = false;
+      connected = true;
+      device.fn.call(device);
+    });
+
+    // identity not acknowledged, try another
+    // identity if there is one
+    io.on('nak', function() {
+      opening = false;
+      self.open();
+
+      // this is a HACK to prevent errors
+      // slowly close down socket, after
+      // enough time has passed for all the
+      // engine.io socketry to be made.
+      setTimeout(function() {
+        io.socket.close();
+      }, 2000);
+    });
+
   });
 
   return this;
 };
+
